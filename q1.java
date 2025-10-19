ab3e48d560e1bfe457124ba42ba84ce2f951c436
Prateek Rai <prateekrai903@gmail.com>
2025-10-19 09:55:27 +0530

    Add files via upload

---
+++ q1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter day number (1–7): ");
+        int day = sc.nextInt();
+
+        String dayName = switch (day) {
+            case 1 -> "Monday";
+            case 2 -> "Tuesday";
+            case 3 -> "Wednesday";
+            case 4 -> "Thursday";
+            case 5 -> "Friday";
+            case 6 -> "Saturday";
+            case 7 -> "Sunday";
+            default -> "Invalid day number!";
+        };
+
+        System.out.println("Day: " + dayName);
+    }
+}
